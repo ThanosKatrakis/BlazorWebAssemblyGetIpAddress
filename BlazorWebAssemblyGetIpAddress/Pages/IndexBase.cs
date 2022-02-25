49c5e01e78647513bf7c6e1f047cba5334216a78
ThanosKatrakis <thanos.katrakis.dev@gmail.com>
2022-02-25 23:37:50 +0200

    Add project files.

---
+++ BlazorWebAssemblyGetIpAddress/Pages/IndexBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorWebAssemblyGetIpAddress.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        public string IpAddress { get; set; } = string.Empty;
+
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await GetMyIpAddress();
+        }
+
+        public async Task<string> GetMyIpAddress()
+        {
+            IpAddress = await JsRuntime.InvokeAsync<string>("getIpAddress")
+                .ConfigureAwait(true);
+
+            return IpAddress;
+        }
+    }
+}
